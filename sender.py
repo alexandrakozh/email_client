6b17cd98be4b6ac7aedabea8ebe87ea7393fda33
alexandrakozh <alexkozhemiakina@gmail.com>
2017-01-19 15:50:16 +0200

    Sender is almost completed

--- sender.py
+++ sender.py
@@ -6,12 +6,11 @@ import os
 from email.mime.text import MIMEText
 import mimetypes
 import email
-from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
-from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from smtplib import SMTP
 import logging
+from email.header import Header
 
 
 def header_type(string):
@@ -23,25 +22,29 @@ def header_type(string):
     return string
 
 
-def defining_subject(headers):
+def get_subject(headers):
     header_subject = None
     for i in headers:
-        if i.startswith("Subject"):
+        if i.lower().startswith("subject"):
             header_subject = i
-        else:
-            continue
+            break
     subject = header_subject.split("=")[-1]
-    log.info('Subject is defined: %s', subject)
     return subject
 
 
-def separating_header_name_and_value(header):
+def handling_header(header):
     header_name, header_value = header.split("=")
-    log.info('Defined: header: %s, header value: %s', header_name, header_value)
+    header_value = Header(header_value, sys.stdin.encoding)
     return header_name, header_value
 
 
-def mail_argument_configuration():
+def read_from_stdin():
+    mail = sys.stdin
+    message = mail.read()
+    return message
+
+
+def mail_argument_configure():
     parser = argparse.ArgumentParser()
     parser.add_argument('smtp_address', action='store',
                         help='Host (ip-address) and port of mail server')
@@ -56,7 +59,7 @@ def mail_argument_configuration():
                         help='Name of user, who send e-mail')
     parser.add_argument('--pwd', action='store', default=None,
                         help='Password of sender')
-    parser.add_argument('--header', action='store', nargs='*', default=[],
+    parser.add_argument('--headers', action='store', nargs='*', default=[],
                         type=header_type,
                         help='Headers of the message in format name=value')
     parser.add_argument('--data', action='store', default=None,
@@ -67,7 +70,7 @@ def mail_argument_configuration():
                         help='A number of copies to send')
     parser.add_argument('--concurrency', action='store', default=1, type=int,
                         help='A number of simultaneous send')
-    parser.add_argument('--send_stdout', action='store_true',
+    parser.add_argument('--stdout', action='store_true',
                         help='Configuring sending an e-mail and redirecting \
                         it to STDOUT')
     parser.add_argument('--attachment_path', action='store', nargs='*',
@@ -83,101 +86,109 @@ class SendingMailError(Exception):
     pass
 
 
-class EmailCreationAndSending(object):
+class EmailCreation(object):
 
-    def __init__(self, smtp_address, mail_from, rcpt_to, tls=False, user=None,
-                 pwd=None, header=None, data=None, data_file=None, count=1,
-                 concurrency=1, send_stdout=False, attachment_path=None):
-        self.smtp_address = smtp_address
-        self.tls = tls
-        self.user = user
-        self.pwd = pwd
+    def __init__(self, mail_from, rcpt_to, user=None, headers=None, data=None,
+                 data_file=None, attachment_path=None):
         self.mail_from = mail_from
-        self.header = header
+        self.rcpt_to = rcpt_to
+        self.user = user
+        self.headers = headers
         self.data = data
         self.data_file = data_file
-        self.count = count
-        self.concurrency = concurrency
-        self.send_stdout = send_stdout
         self.attachment_path = attachment_path
-        self.rcpt_to = rcpt_to
-        self.message = None
-        self.subject = None
+        self._message = None
+        self._subject = None
 
-    def reading_from_stdin(self):
-        email_file = sys.stdin
-        self.message = email_file.read()
-        return self.message
+    @property
+    def message(self):
+        return self._message
 
-    def message_creation(self):
+    @property
+    def subject(self):
+        return self._subject
+
+    def generate_message(self):
         if len(self.attachment_path) == 1:
             if self.data is None and self.data_file is None:
                 log.info(u'Creating singlepart message with one attachment \
-                          and no data is started')
-                return self.creating_singlepart_msg()
+                          and no data')
+                return self.create_singlepart_msg()
             else:
                 log.info(u'Creating multipart message with one attachment \
-                          and data is started')
-                return self.creating_multipart_msg()
+                          and data')
+                return self.create_multipart_msg()
         elif len(self.attachment_path) > 1:
-            log.info(u'Creating multipart message is started')
-            return self.creating_multipart_msg()
+            log.info(u'Creating multipart message')
+            return self.create_multipart_msg()
         else:
-            log.info(u'Creating simple singlepart message is started')
-            return self.creating_singlepart_msg()
+            log.info(u'Creating simple singlepart message')
+            return self.create_singlepart_msg()
 
-    def creating_singlepart_msg(self):
-        self.subject = defining_subject(self.header)
+    def create_singlepart_msg(self):
+        self._subject = get_subject(self.headers)
+        log.info('Subject: %s', self.subject)
         if len(self.attachment_path) > 0:
-            self.message = MIMEMultipart()
-            self.message['Subject'] = self.subject
-            self.message['From'] = self.user
-            self.message['To'] = ", ".join(self.rcpt_to)
-            if len(self.header) > 0:
-                for header in self.header:
-                    header, value = separating_header_name_and_value(header)
-                    self.message[header] = value
-            if os.path.isfile(self.attachment_path[0]):
-                self.attaching_files_to_message(self.attachment_path[0])
+            attachment = self.attachment_path[0]
+            with open(attachment, 'rb') as f:
+                content_type, encoding = mimetypes.guess_type(f)
+                maintype, subtype = content_type.split('/', 1)
+                msg = MIMEBase(maintype, subtype)
+                msg.set_payload(f.read())
+            email.encoders.encode_base64(msg)
+            msg.add_header("Content-Disposition", 'attachment',
+                           filename=attachment)
+            self.add_headers_to_msg()
+
+            if os.path.isfile(attachment):
+                log.info(u'File %s is attaching to message',
+                         attachment)
+                self.attach_files_to_message(attachment)
             return self.message
 
         if self.data_file is not None:
             with open(self.data_file, 'rb') as fp:
-                self.message = MIMEText(fp.read())
+                self._message = MIMEText(fp.read())
         elif self.data is not None:
-            self.message = MIMEText(self.data)
-        self.message['Subject'] = self.subject
-        self.message['From'] = self.user
-        self.message['To'] = ", ".join(self.rcpt_to)
+            self._message = MIMEText(self.data)
+        self.add_headers_to_msg()
         return self.message
 
-    def creating_multipart_msg(self):
-        self.subject = defining_subject(self.header)
-        self.message = MIMEMultipart()
-        self.message['Subject'] = self.subject
-        self.message['From'] = self.user
-        self.message['To'] = ", ".join(self.rcpt_to)
-        if len(self.header) > 0:
-            for header in self.header:
-                header, value = separating_header_name_and_value(header)
-                self.message[header] = value
+    def create_multipart_msg(self):
+        self._subject = get_subject(self.headers)
+        log.info('Subject: %s', self.subject)
+        self._message = MIMEMultipart()
+        self.add_headers_to_msg()
 
-        if self.data_file is not None:
+        if self.data_file is not None and self.data is not None:
+            self.attachment_path.append(self.data_file)
+        elif self.data_file is not None and self.data is None:
             with open(self.data_file, 'rb') as fp:
                 text = fp.read()
                 msg = MIMEText(text)
                 self.message.attach(msg)
-        elif self.data is not None:
+        elif self.data is not None and self.data_file is None:
             msg = MIMEText(self.data)
             self.message.attach(msg)
 
         for attachment in self.attachment_path:
             if not os.path.isfile(attachment):
+                log.warning(u'File %s is not found', attachment)
                 continue
-            self.attaching_files_to_message(attachment)
+            self.attach_files_to_message(attachment)
         return self.message
 
-    def attaching_files_to_message(self, attachment_file):
+    def add_headers_to_msg(self):
+        self.message['From'] = self.user
+        self.message['To'] = ", ".join(self.rcpt_to)
+        if len(self.headers) > 0:
+            for header in self.headers:
+                header, value = handling_header(header)
+                log.info('Header: %s, header-value: %s', header, value)
+                self.message[header] = value
+        return self.message
+
+    def attach_files_to_message(self, attachment_file):
         try:
             content_type, encoding = mimetypes.guess_type(attachment_file)
             maintype, subtype = content_type.split('/', 1)
@@ -194,48 +205,73 @@ class EmailCreationAndSending(object):
                                         Please try again!")
         return self.message
 
-    def send_mail(self):
-        server = SMTP(self.smtp_address)
+
+class EmailTransport(object):
+
+    def __init__(self, smtp_address, mail_from, rcpt_to, tls=False, user=None,
+                 pwd=None, count=1, concurrency=1):
+        self.smtp_address = smtp_address
+        self.mail_from = mail_from
+        self.rcpt_to = rcpt_to
+        self.tls = tls
+        self.user = user
+        self.pwd = pwd
+        self.count = count
+        self.concurrency = concurrency
+        self.server = None
+
+    def server_connect_and_login(self):
+        self.server = SMTP(self.smtp_address)
         log.info(u'Client is connected to server')
-        server.ehlo()
+        self.server.ehlo()
         if self.tls:
-            server.starttls()
-        server.login(self.mail_from, self.pwd)
+            log.info(u'TLS is started')
+            self.server.starttls()
+        self.server.login(self.mail_from, self.pwd)
         log.info(u'Username and password are correct')
+        return self.server
+
+    def send_mail(self, message):
         try:
-            server.sendmail(self.mail_from, self.rcpt_to,
-                            self.message.as_string())
-            log.info(u'Your message was successfully send! Congratulations!!!')
+            self.server.sendmail(self.mail_from, self.rcpt_to,
+                                 message.as_string())
+            log.info(u'Your message was successfully send!')
         except Exception:
             log.error(u'Sending Mail Error is raised')
             raise SendingMailError("Message cannot be sent!")
-        finally:
-            server.quit()
+        return self.server
+
+    def server_disconnect(self):
+        log.info(u'Server is disconnecting')
+        self.server.quit()
 
 
 def main():
-    parser = mail_argument_configuration()
+    parser = mail_argument_configure()
     args = parser.parse_args()
-    log.info('%s', args)
-    mail = EmailCreationAndSending(args.smtp_address, args.mail_from,
-                                   args.rcpt_to, args.tls, args.user, args.pwd,
-                                   args.header, args.data, args.data_file,
-                                   args.count, args.concurrency,
-                                   args.send_stdout, args.attachment_path)
+    log.info('Arguments: %s', args)
+    message = EmailCreation(args.mail_from, args.rcpt_to, args.user,
+                            args.headers, args.data, args.data_file,
+                            args.attachment_path)
+
     if not sys.stdin.isatty():
         log.info(u'The message is being read from stdin')
-        mail.reading_from_stdin()
+        message = read_from_stdin()
     else:
         log.info(u'The message is being created')
-        mail.message_creation()
-    if args.send_stdout:
+        message = message.generate_message()
+
+    if args.stdout:
         log.info(u'The message is being sent to stdout')
-        message = mail.message_creation()
         print message
     else:
         log.info(u'The message is being sent to recipient')
-        mail.send_mail()
-
+        mail = EmailTransport(args.smtp_address, args.mail_from, args.rcpt_to,
+                              args.tls, args.user, args.pwd, args.count,
+                              args.concurrency)
+        mail.server_connect_and_login()
+        mail.send_mail(message)
+        mail.server_disconnect()
 
 if __name__ == "__main__":
     FORMAT = u'%(asctime)s - %(levelname)s - %(message)s'
@@ -243,4 +279,3 @@ if __name__ == "__main__":
                         level=logging.DEBUG)
     log = logging.getLogger(__name__)
     main()
-
--- test_parsing.py
+++ test_parsing.py
@@ -3,6 +3,8 @@ import sender
 from collections import OrderedDict
 from mock import MagicMock, patch
 from argparse import ArgumentParser
+import logging
+
 
 all_args_for_list = [
     ('_smtp_address', 'smtp_gmail.com:587'),
@@ -12,8 +14,8 @@ all_args_for_list = [
     ('data_file', '/tmp/text.txt'),
     ('attachment_path', ['1', '2']),
     ('count', 2),
-    ('send_stdout', True),
-    ('header', ['Content-type=text/plain', 'Subject=Hello']),
+    ('stdout', True),
+    ('headers', ['Content-type=text/plain', 'Subject=Hello']),
     ('pwd', '123'),
     ('user', 'Alexandra Kozhemiakina'),
     ('concurrency', 5),
@@ -26,8 +28,8 @@ default_args = [
     ('data_file', None),
     ('attachment_path', []),
     ('count', 1),
-    ('send_stdout', False),
-    ('header', []),
+    ('stdout', False),
+    ('headers', []),
     ('pwd', None),
     ('user', None),
     ('concurrency', 1),
@@ -43,6 +45,42 @@ required_args = [
     ]
 required_args = OrderedDict(required_args)
 
+one_attachment_no_data = [
+    ('_smtp_address', 'smtp_gmail.com:587'),
+    ('mail_from', 'foo@mail.net'),
+    ('rcpt_to', ['bar@mail.net', 'baz@mail.net']),
+    ('attachment_path', ['1', '2']),
+    ('data', None),
+    ('data_file', None)
+    ]
+one_attachment_no_data = OrderedDict(one_attachment_no_data)
+
+one_attachment_and_data = [
+    ('_smtp_address', 'smtp_gmail.com:587'),
+    ('mail_from', 'foo@mail.net'),
+    ('rcpt_to', ['bar@mail.net', 'baz@mail.net']),
+    ('attachment_path', ['1']),
+    ('data', 'Hi!')
+    ]
+one_attachment_and_data = OrderedDict(one_attachment_and_data)
+
+many_attachments = [
+    ('_smtp_address', 'smtp_gmail.com:587'),
+    ('mail_from', 'foo@mail.net'),
+    ('rcpt_to', ['bar@mail.net', 'baz@mail.net']),
+    ('attachment_path', ['1', '2', '3']),
+    ('data', 'Hi!'),
+    ('data_file', '1.txt')
+    ]
+many_attachments = OrderedDict(many_attachments)
+
+one_data = [
+    ('_smtp_address', 'smtp_gmail.com:587'),
+    ('mail_from', 'foo@mail.net'),
+    ('rcpt_to', ['bar@mail.net', 'baz@mail.net']),
+    ('data', 'Hi!')
+    ]
+one_data = OrderedDict(one_data)
 
 def dict_to_cmd_list(d):
     res = []
@@ -68,19 +106,19 @@ def arg_by_key(arg):
 
 class TestEmailClient(unittest.TestCase):
 
-    def test_mail_argument_configuring_default_values(self):
-        parser = sender.mail_argument_configuration()
+    def test_mail_argument_configure_default_values(self):
+        parser = sender.mail_argument_configure()
         res = parser.parse_args(dict_to_cmd_list(required_args))
         for key, value in default_args.items():
             self.assertEqual(getattr(res, key), default_args[key])
 
-    def test_mail_argument_configuring_all_values(self):
-        parser = sender.mail_argument_configuration()
+    def test_mail_argument_configure_all_values(self):
+        parser = sender.mail_argument_configure()
         res = parser.parse_args(dict_to_cmd_list(all_args_for_list))
         for key, value in all_args_for_list.items():
             self.assertEqual(getattr(res, arg_by_key(key)), all_args_for_list[key])
 
-    def test_argument_configuring_without_positionals(self):
+    def test_argument_configure_without_positionals(self):
         i = 0
         while i < 3:
             args_without_one_positional = []
@@ -91,11 +129,39 @@ class TestEmailClient(unittest.TestCase):
                 args_without_one_positional.append(tup)
             args_without_one_positional = OrderedDict(args_without_one_positional)
             with patch.object(ArgumentParser, 'exit') as mock_method:
-                parser = sender.mail_argument_configuration()
+                parser = sender.mail_argument_configure()
                 parser.parse_args(dict_to_cmd_list(args_without_one_positional))
                 mock_method.assert_called()
             i += 1
 
+    def test_generate_message_singlepart(self):
+        for d in (one_attachment_no_data, one_data):
+            with patch.object(sender.EmailCreation, 'create_singlepart_msg') as mock_method:
+                parser = sender.mail_argument_configure()
+                args = parser.parse_args(dict_to_cmd_list(d))
+                m = sender.EmailCreation(args.mail_from, args.rcpt_to, args.user,
+                                     args.headers, args.data, args.data_file,
+                                     args.attachment_path)
+
+                mock_method.assert_called()
+
+    def test_generate_message_multipart(self):
+        for i in (one_attachment_and_data, many_attachments):
+            with patch.object(sender.EmailCreation, 'create_multipart_msg') as mock_method:
+                parser = sender.mail_argument_configure()
+                args = parser.parse_args(dict_to_cmd_list(i))
+                m = sender.EmailCreation(args.mail_from, args.rcpt_to, args.user,
+                                     args.headers, args.data, args.data_file,
+                                     args.attachment_path)
+                message = m.generate_message()
+                mock_method.assert_called()
+
+        
+
 
 if __name__ == '__main__':
+    FORMAT = u'%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=FORMAT, filename=u'email.log',
+                        level=logging.DEBUG)
+    log = logging.getLogger(__name__)
     unittest.main()
